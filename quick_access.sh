916c08cffabdfefaceb9572b98290da1b84439b1
BlancaCC <blancacano2.71828@gmail.com>
2021-10-07 00:04:02 +0200

    P1: ejercicio 1 peleandome con las tablas

---
+++ quick_access.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Abrir carpeta de material
+# nautilus -w /home/blanca/repositorio/TFG &
+
+# Abrir el código fuente en vscode
+code  /home/blanca/repositorio/Computer-vision/practica_1/practica_1.ipynb &
+
+
+# Abrir pdfs
+evince /home/blanca/repositorio/Computer-vision/practica_1/guiones/P1.pdf &
+evince /home/blanca/repositorio/Computer-vision/practica_1/guiones/practica1-2021.v3.pdf  &
